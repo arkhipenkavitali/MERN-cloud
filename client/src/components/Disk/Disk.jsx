df2a52880d6393bd4739a9f5b0e3d3e5afdd3c97
v.arkhipenka <otake@kontrotake.gg>
2021-05-28 17:13:48 +0300

    add folder navigation(stack)

--- client/src/components/Disk/Disk.jsx
+++ client/src/components/Disk/Disk.jsx
@@ -1,23 +1,29 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {createDir, getFiles} from "../../actions/file";
+import {getFiles} from "../../actions/file";
 import FileList from "./fileList/FileList";
 import './Disk.less'
 import Popup from "./Popup";
-import {setPopupDisplay} from "../../reducers/fileReducer";
+import {setCurrentDir, setPopupDisplay} from "../../reducers/fileReducer";
 
 const Disk = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
+    const dirStack = useSelector(state => state.files.dirStack)
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
     }, [currentDir])
 
+    function backClickHandler() {
+        const backDirId = dirStack.pop()
+        dispatch(setCurrentDir(backDirId))
+    }
+
     return (
         <div className="disk">
             <div className="disk__btns">
-                <button className="disk__back">Назад</button>
+                {currentDir && <button className="disk__back" onClick={() => backClickHandler()}>Назад</button>}
                 <button className="disk__create" onClick={() => dispatch(setPopupDisplay('flex'))}>Создать папку</button>
             </div>
             <FileList/>
